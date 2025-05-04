85c3f1506ed93b933f98999d7726d3c368b557aa
samuhs <samuelhs98@gmail.com>
2025-05-03 22:18:52 -0300

    fix(setup): complete setups infos

--- setup.py
+++ setup.py
@@ -6,17 +6,17 @@ setup(
     author = 'Samuel Silva',
     author_email = 'samuel.silva@gmail.com',
     packages = ['chop_doc'],
-    description = 'A easy way to split documentos in chuncks',
-    long_description = 'A easy way to split documentos in chuncks '
+    description = 'A easy way to split documents in chunks',
+    long_description = 'A easy way to split documents in chunks '
                         + 'build with langchain compatible ',
                         + 'to help you chop your docs',
-    url = 'https://github.com/yanorestes/aluratemp',
+    url = 'https://github.com/samuhs/chop_doc',
     project_urls = {
-        'Código fonte': 'https://github.com/yanorestes/aluratemp',
-        'Download': 'https://github.com/yanorestes/aluratemp/archive/1.0.0.zip'
+        'Código fonte': 'https://github.com/samuhs/chop_doc',
+        'Download': 'https://github.com/samuhs/chop_doc/archive/0.1.0.zip'
     },
     license = 'MIT',
-    keywords = 'conversor temperatura alura',
+    keywords = 'chunking langchain easy',
     classifiers = [
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
